abfd7f2e6cdd79274e54c5adf4769935e8272177
MCasteras <mathieu.casteras@gmail.com>
2020-10-12 15:40:04 +0300

    V 0.5

--- safetylinespackage/__init__.py
+++ safetylinespackage/__init__.py
@@ -8,4 +8,4 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, accuracy_score
 from sklearn.metrics import confusion_matrix
 
-from safetylinespackage import generate,get_metric,learn,learn,predict,target_statistics,features_statistics,correlation,statistics,print_correlations
+from safetylinespackage.safetylinespackage import generate,get_metric,learn,learn,predict,target_statistics,features_statistics,correlation,statistics,print_correlations
